61fba91d87cf3e6a2036cf2dd66c3502d54acd6c
FijiAura <sebastiancatania@icloud.com>
2025-11-02 16:09:07 +0800

    vibe coded fix bru

--- src/rooms/roblox.hpp
+++ src/rooms/roblox.hpp
@@ -2,12 +2,14 @@
 #include <Geode/Geode.hpp>
 #include <Geode/binding/FMODAudioEngine.hpp>
 #include "../funcs.hpp"
+#include <random>
 using namespace geode::prelude;
 
 namespace rooms {
-    class basement_stuff : public cocos2d::CCNode {
+    class roblox_stuff : public cocos2d::CCNode {
     public:
         cocos2d::CCLayer* parent = nullptr;
+
         void drake(CCObject*) {
             auto win = CCDirector::get()->getWinSize();
             if (parent) {
@@ -20,11 +22,12 @@ namespace rooms {
                 FLAlertLayer::create("Drake", "<cr>" + message + "</c>", "OK")->show();
             }
         }
+
         void diddy(CCObject*) {
             auto win = CCDirector::get()->getWinSize();
             if (parent) {
                 std::vector<std::string> messages = {
-                    "*diddles you*"
+                    "*diddles you*",
                     "*gets oiled up*"
                 };
                 std::string message = messages[rand() % messages.size()];
@@ -42,7 +45,6 @@ namespace rooms {
                 FLAlertLayer::create("Tienes 14?", "<cr>" + message + "</c>", "OK")->show();
             }
         }
-
     };
 
     inline cocos2d::CCMenu* theroblox(cocos2d::CCLayer* parent = nullptr) {
@@ -51,27 +53,19 @@ namespace rooms {
         things->parent = parent;
         devlin::set_emotion(parent, "wth.gif"_spr, 3.0f, 0.3f, 0.2f);
 
-        auto dr = CCSprite::create("drake.png"_spr);
-        dr->setScale(2.5f);
-        auto drake = CCMenuItemSpriteExtra::create(dr, nullptr, things, menu_selector(roblox_stuff::drake));
+        auto drakeSprite = CCSprite::create("drake.png"_spr);
+        drakeSprite->setScale(2.5f);
+        auto drake = CCMenuItemSpriteExtra::create(drakeSprite, nullptr, things, menu_selector(roblox_stuff::drake));
         drake->setPosition({win.width * 0.65f, win.height / 4.20f});
         drake->setID("drake"_spr);
 
-        auto diddy = CCMenuItemSpriteExtra::create(dr, nullptr, things, menu_selector(roblox_stuff::diddy));
-        drake->setPosition({win.width - 120.f, win.height / 1.5f + 69.f});
-        drake->setID("diddy"_spr);
+        auto diddySprite = CCSprite::create("diddy.png"_spr);
+        diddySprite->setScale(2.5f);
+        auto diddy = CCMenuItemSpriteExtra::create(diddySprite, nullptr, things, menu_selector(roblox_stuff::diddy));
+        diddy->setPosition({win.width - 120.f, win.height / 1.5f + 69.f});
+        diddy->setID("diddy"_spr);
 
-        auto advy = CCMenuItemSpriteExtra::create(dr, nullptr, things, menu_selector(roblox_stuff::diddy));
-        drake->setPosition({win.width / 12.f, win.height / 2.f});
-        drake->setID("advy"_spr);
-
-
-        auto menu = CCMenu::create();
-        menu->setPosition({0,0});
-        menu->addChild(drake);
-        menu->addChild(diddy);
-        menu->addChild(advy);
-        menu->setID("room-menu"_spr);
-        return menu;
-    }
-}
+        auto advySprite = CCSprite::create("advy.png"_spr);
+        advySprite->setScale(2.5f);
+        auto advy = CCMenuItemSpriteExtra::create(advySprite, nullptr, things, menu_selector(roblox_stuff::advy));
+        advy->setPosition({win.width / 
